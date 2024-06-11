277935e19a1b8b668fdf370109a2a84d0e9d08b8
Ah112 <ah112233279@yahoo.com>
2024-06-11 16:15:33 +0300

    final commet

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# from flask import Flask, request, jsonify
-# from werkzeug.utils import secure_filename
-# import os
-# import json
-# from werkzeug.serving import run_simple
-#
-# from Model_ml import JobRecommender
-#
-# app = Flask(__name__)
-#
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file part in the request'}), 400
-#
-#     file = request.files['file']
-#     if file.filename == '':
-#         return jsonify({'error': 'No file selected for uploading'}), 400
-#
-#     if file:
-#         filename = secure_filename(file.filename)
-#         file.save(filename)  # replace with your actual path
-#         recommender = JobRecommender()
-#         recommendations = recommender.process_cv(filename)  # replace with your actual path
-#
-#         return jsonify(recommendations)
-# if __name__ == '__main__':
-#     run_simple('localhost', 5000, app)
 
 
 from flask import Flask, request, jsonify
